b1f323b8a4a8d44b04176002dec94e8d5da8f761
Digvijay-Kadam <digvijay.kadam@bluecrestsoft.com>
2024-03-24 20:41:09 +0530

    added loader

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,18 +1,31 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 import QuickItem from './home/QuickItem/QuickItem';
 import Wallpaper from './home/Wallpaper'
 
 export default function Home() {
-  const [loading, setLoading] = useState(true)
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    // Simulating loading delay for demonstration purposes
+    const timer = setTimeout(() => {
+      setLoading(false);
+    }, 2000);
+
+    // Clean up the timer on component unmount
+    return () => clearTimeout(timer);
+  }, []);
+
   return (
     <>
-
-      <Wallpaper />
-  
-      <QuickItem />
-
-
+      {loading ? (
+        <div className='m-5 text-center '>Loading...</div> // You can replace this with your loader component
+      ) : (
+        <>
+          <Wallpaper />
+          <QuickItem />
+        </>
+      )}
     </>
   );
 }
